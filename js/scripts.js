c3140048fd0d38ff44821eba2e24d7e75ee63d38
Joseph Golden <omniint2@protonmail.com>
2020-02-23 14:06:45 -0800

    Declared variables in the js file and modified the form id within the html file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
+  $("#program").submit(function() {
+    var ruby = 0;
+    var c = 0;
+    var js = 0;
+    var python = 0;
+    var swift = 0;
+    var html = 0;
+
+    let nameInput = $("input#name").val();
+    let fieldInput = $("input:radio[name=field]:checked").val();
+    let osInput = $("input:radio[name=os]:checked").val();
+    let levelInput = $("input:radio[name=level]:checked").val();
+    let infoInput = $("input:radio[name=info]:checked").val();
+    let areaInput = $("input:radio[name=area]:checked").val();
+
+    
+
+    
+
+  })
   var program = ["question1", "question2", "question3", "question4", "question5"];
 
   program.forEach(function(blank) {
