efff2219015205a8e2ab25aee78c5df973958ffc
Joseph Golden <omniint2@protonmail.com>
2020-02-23 14:48:04 -0800

    Added the first result depending on the survey options selected within the html file.

--- js/scripts.js
+++ js/scripts.js
@@ -14,16 +14,18 @@ $(document).ready(function() {
     let infoInput = $("input:radio[name=info]:checked").val();
     let areaInput = $("input:radio[name=area]:checked").val();
 
-    
-
-    
-
-  })
-  var program = ["question1", "question2", "question3", "question4", "question5"];
-
-  program.forEach(function(blank) {
-    var userInput = $("input." + program).val();
-    console.log(userInput);
-    $("." + program).text(userInput).val();
+    if(ruby > c && ruby > js && ruby > python && ruby > swift && ruby > html) {
+      $(".ruby").fadeIn();
+    } else if(c > ruby && c > js && c > python && c > swift && c > html) {
+      $(".c#").fadeIn();
+    } else if(js > ruby && js > c && js > python && js > swift && js > html) {
+      $(".js").fadeIn();
+    } else if(python > ruby && python > c && python > js && python > swift && python > html) {
+      $(".python").val();
+    } else if(swift > ruby && swift > c && swift > js && swift > python && swift > html) {
+      $(".swift").val();
+    } else if(html > ruby && html > c && html > js && html > python && html > swift) {
+      $(".html").val();
+    }
   });
-})
+});
