1f1fa04b7bee7f047804bcc4d8533aa22bdb6a5d
Joseph Golden <omniint2@protonmail.com>
2020-02-22 18:36:55 -0800

    Concluded 5 question segment of html while modifying styles.css and script files.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var program = ["question1", "question2", "question3"];
+  var program = ["question1", "question2", "question3", "question4", "question5"];
 
   program.forEach(function(blank) {
     var userInput = $("input." + program).val();
